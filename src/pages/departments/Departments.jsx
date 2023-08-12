3c7bb891d2c14bb7081d6ce2b6bcec08c6f9e7b5
Vivek Prasad <flashtech007@gmail.com>
2023-08-12 21:51:03 +0530

    added filter dispatch to departments page

--- src/pages/departments/Departments.jsx
+++ src/pages/departments/Departments.jsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import "./Departments.css"
 import { Sidebar } from '../../components/sidebar/Sidebar'
+import { useNavigate } from 'react-router-dom'
+import { useAppContext } from '../../contexts/AppContext'
 
 export const Departments = () => {
+    const {dispatch} = useAppContext();
+    const navigate = useNavigate({});
   return (
     <div className='departments'>
         <Sidebar/>
         <div className='department-area'>
 
-            <div className='department-unit'>
+            <div 
+            onClick={() => {
+                dispatch({ type: "FILTER_BY_DEPARTMENT", payload: "Kitchen" });
+                navigate("/products")
+            }}
+            className='department-unit' >
                 <h2>Kitchen</h2>
             </div>
 
-            <div className='department-unit'>
+            <div 
+            onClick={() => {
+                dispatch({ type: "FILTER_BY_DEPARTMENT", payload: "Clothing" });
+                navigate("/products")
+            }}
+            className='department-unit'>
                 <h2>Clothing</h2>
             </div>
 
-            <div className='department-unit'>
+            <div
+            onClick={() => {
+                dispatch({ type: "FILTER_BY_DEPARTMENT", payload: "Toys" });
+                navigate("/products")
+            }}
+            className='department-unit'>
                 <h2>Toys</h2>
             </div>
 
